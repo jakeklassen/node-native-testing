af9c3a3db12fd453d1af6499d09601a5bbc6dcfc
Jake Klassen <jklassendev@gmail.com>
2023-03-21 16:37:06 -0400

    messing with new test output

--- lib/math.test.ts
+++ lib/math.test.ts
@@ -1,9 +1,17 @@
 import * as assert from "node:assert";
 import { describe, it } from "node:test";
-import { sum } from "./math.js";
+import { multiply, sum } from "./math.js";
 
-describe("sum", () => {
-  it("adds 1 + 2 to equal 3", () => {
-    assert.strictEqual(sum(1, 2), 3);
+describe("math", () => {
+  describe("sum", () => {
+    it("adds 1 + 2 to equal 3", () => {
+      assert.strictEqual(sum(1, 2), 3);
+    });
+  });
+
+  describe("multiply", () => {
+    it("multiplies 2 * 3 to equal 6", () => {
+      assert.strictEqual(multiply(2, 3), 6);
+    });
   });
 });
--- lib/math.ts
+++ lib/math.ts
@@ -1,1 +1,4 @@
 export const sum = (...numbers: number[]) => numbers.reduce((a, b) => a + b, 0);
+
+export const multiply = (...numbers: number[]) =>
+  numbers.reduce((a, b) => a * b, 1);
